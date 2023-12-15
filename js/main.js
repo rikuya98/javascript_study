1153f5c1851d779eef139c7bc1f1261dfb53ca71
日下陸矢 <rikuya.98@gmail.com>
2023-12-15 18:41:40 +0900

    SVGでメータを作成、インクリメントに合わせてメータを上下

--- js/main.js
+++ js/main.js
@@ -10,16 +10,35 @@
 //     }
 // })
 // app.mount('#app')
-  
+const INCREMENT = 1
+const DECREMENT = -1
+
+function updateMeter(counterValue) {
+    const meter = document.querySelector('#meter');
+    const maxWidth = 200;
+    const newWidth = (maxWidth / 10) * counterValue;
+    meter.setAttribute('width', newWidth);
+}
 
 const counterElement = document.querySelector('#counter');
 
 function updateCounter(amount) {
-    counterElement.textContent = parseInt(counterElement.textContent) + amount
+    let currentValue = parseInt(counterElement.textContent);
+
+    if (amount > 0 && currentValue < 10) {
+        currentValue += amount;
+    }
+    else if (amount < 0 && currentValue > 0 ) {
+        currentValue += amount;
+    }
+
+    counterElement.textContent = currentValue
+    updateMeter(currentValue)
 }
 
 const countUpButton = document.querySelector('#countup');
-countUpButton.addEventListener('click', () => updateCounter(1));
+countUpButton.addEventListener('click', () => updateCounter(INCREMENT));
 
 const countDownButton = document.querySelector('#countdown');
-countDownButton.addEventListener('click', () => updateCounter(-1));
+countDownButton.addEventListener('click', () => updateCounter(DECREMENT));
+
