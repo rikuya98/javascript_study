266226d9158d176084b8b28c427e7d5d21c1d3ff
日下陸矢 <rikuya.98@gmail.com>
2023-12-15 16:05:01 +0900

    カウントアップ機能の追加

--- js/main.js
+++ js/main.js
@@ -1,15 +1,25 @@
-const app = Vue.createApp({
-    data: () => ({
-        message: 'Hello'
+// const app = Vue.createApp({
+//     data: () => ({
+//         message: 'Hello'
  
-    }),
-    methods: {
-        clickHandler: function(event) {
-            this.message = this.message.split('').reverse().join('')
-        }
-    }
-})
-app.mount('#app')
+//     }),
+//     methods: {
+//         clickHandler: function(event) {
+//             this.message = this.message.split('').reverse().join('')
+//         }
+//     }
+// })
+// app.mount('#app')
+  
 
+const counterElement = document.querySelector('#counter');
 
+function updateCounter(amount) {
+    counterElement.textContent = parseInt(counterElement.textContent) + amount
+}
 
+const countUpButton = document.querySelector('#countup');
+countUpButton.addEventListener('click', () => updateCounter(1));
+
+const countDownButton = document.querySelector('#countdown');
+countDownButton.addEventListener('click', () => updateCounter(-1));
